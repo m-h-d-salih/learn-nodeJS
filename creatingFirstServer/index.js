f7b50b65994b304429602118d24bdf672644dd74
m-h-d-salih <salihsha656@gmail.com>
2024-08-29 14:27:19 +0530

    serving html file

--- creatingFirstServer/index.js
+++ creatingFirstServer/index.js
@@ -1,6 +1,11 @@
 var http=require('http')
+var fs=require('fs')
 http.createServer((req,res)=>{//req= request object,res=response object
-    
-    res.write("first server")
-    res.end()
+    fs.readFile('index.html',(error,data)=>{
+        
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write(data)
+        res.end()
+    })
+   
 }).listen(8000)//port number
