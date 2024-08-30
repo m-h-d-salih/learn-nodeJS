ab4378d78f95f5231de4245cbefe41be2f3ae9aa
m-h-d-salih <salihsha656@gmail.com>
2024-08-30 16:45:35 +0530

    event off

--- eventModule/eventmodule.js
+++ eventModule/eventmodule.js
@@ -7,3 +7,29 @@ console.log('user logged in')
 })
 //event emission
 emitter.emit('login');//triggering an event
+
+//what if i need to pass parameters 
+
+emitter.on('order',({item,price})=>{//this function is call when the specific event is triggered
+    console.log(`you are ordered ${item} that cost '$ ${price} `)
+    })
+    emitter.emit('order',{item:'pizza',price:200})
+
+    //what if i give deffrent function to same event
+    //ans: it will deffrentiate with the parameter
+emitter.on('order',({item})=>console.log(`u ordered ${item}`))
+emitter.emit('order',{item:'pizza'})
+emitter.once('logout',()=>{//this function will work once 
+    console.log('user logged out')
+    })
+emitter.emit('logout') //this will work
+emitter.emit('logout')// but this is not going to work
+//what if  i need to remove a specific event and its method
+
+// const welcome=(user)=>{
+//     console.log(`welcome ${user}` )
+// }
+// emitter.on('welcome',welcome)
+// emitter.emit('welcome',"salih")
+// emitter.off('welcome');
+// emitter.emit('welcome',"salih")// this wont work
