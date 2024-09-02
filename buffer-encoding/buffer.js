9932b86424eb5a4859d125f2a085b4461c195766
m-h-d-salih <salihsha656@gmail.com>
2024-09-02 20:04:30 +0530

    stream read wrrite

--- buffer-encoding/buffer.js
+++ buffer-encoding/buffer.js
@@ -1,4 +1,17 @@
-const buffer=new Buffer.from('salih')
-console.log(buffer)
-console.log(buffer.toJSON()) 
+// const buffer=new Buffer.from('salih')
+// console.log(buffer)
+// console.log(buffer.toJSON()) 
 
+
+const fs=require('fs')
+const readableStream=fs.createReadStream('./file1.txt',{encoding:'utf-8',highWaterMark:2})
+const writableStream=fs.createWriteStream('./file2.txt')
+readableStream.on("data",(chunk)=>{
+    console.log(chunk)
+    writableStream.write(chunk)
+})
+writableStream.write('\n hello')
+writableStream.on('finish', () => {
+    console.log('Finished writing to the file');
+  });
+  writableStream.emit('finish')
