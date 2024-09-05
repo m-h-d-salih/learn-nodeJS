db504e904e51f0e4c4d089b3442ad9b2a6ce1ebf
m-h-d-salih <salihsha656@gmail.com>
2024-09-05 12:06:40 +0530

    os module

--- eventLOOP/executionOrderInQueses.js
+++ eventLOOP/executionOrderInQueses.js
@@ -19,3 +19,23 @@ console.log(`main`)
 // microstask:promise
 // timer: settimeoout
 // check : setimmedatte
+
+//queueMicrotask()
+
+
+queueMicrotask(()=>{
+    console.log(`microtask : queueMicroTask`)
+})
+Promise.resolve().then(()=>{
+    console.log(`microstask:promise`)
+})
+process.nextTick(()=>{
+    console.log(`microtask :process.nextTick`)
+})
+// expected output
+
+// microtask :process.nextTick
+// microtask :process.nextTick
+// microstask:promise
+// microtask : queueMicroTask
+// microstask:promise
---
+++ osmodule/osmodule.js
@@ -0,0 +1,3 @@
+const os = require('os');
+console.log(`total memory ${os.totalmem}`)
+console.log(`free memory ${os.freemem}`)
