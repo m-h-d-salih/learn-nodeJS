1d6d6e0ea484f6a7ade43fdf2e7a76ae2d82a8f5
m-h-d-salih <salihsha656@gmail.com>
2024-10-27 12:50:59 +0530

    apply

--- js/callapplybind.js
+++ js/callapplybind.js
@@ -1,6 +1,11 @@
+//call
 function greet(greeting){
     console.log(`${greeting} ${this.name}`)
 }
 
 const person={name:'salih'}
 greet.call(person,"hello");
+
+//apply
+
+greet.apply(person,['hii'])
